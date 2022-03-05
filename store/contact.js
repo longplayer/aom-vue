1d9913af96763a3d64b28c3df1abcdcd607f16e3
longplayer <bartaber@hotmail.com>
2022-03-05 14:56:14 +0100

    Removed contact data

--- store/contact.js
+++ store/contact.js
@@ -1,6 +1,7 @@
-import contact from '@/data/contact.js'
-
-export const state = () => contact
+export const state = () => ({
+  title: '',
+  button: ''
+})
 
 export const getters = {
   getContact: (state) => state,
