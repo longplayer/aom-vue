c5da5d39c8d68ffd14b9c33698a9908bf7a48dc1
longplayer <bartaber@hotmail.com>
2022-04-06 10:12:25 +0200

    Added google bot meta + updated page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Age-of-Robots',
+    title: 'Age of Robots | Miguel Fernandez',
     htmlAttrs: {
       lang: 'fr',
     },
@@ -15,6 +15,7 @@ export default {
       { hid: 'description', name: 'description', content: 'Présentation de mes travaux artistiques. Artiste résidant à Bruxelles dans le quartier des Marolles près du Vieux Marché aux Puces. Je crée des peintures et des sculptures de robots inspirés par la science-fiction et fait de matériaux de récupération.' },
       { name: 'keywords', lang: 'fr', content: 'Miguel, Fernandez, Miguel Fernandez, art de miguel fernandez, age of robots, peinture, peintures, pictural, toile, tableau, sculpture, sculptures, robot, robots, kitbash, expo, exposition, expositions, art, artiste, artiste peintre, sculpteur, a normal graffizm, (A)normal Graffizm, recyclage, recuperation, objets, fernandes, fernandez, of, fernandez'},
       { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'google-site-verification', name: 'google-site-verification', content: 'KZauwPn6Y-hvpFsfa-FNaiRYaMFrS74agsULk5UbG_I' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
